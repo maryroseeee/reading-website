1b1feecb4a4a6cca89a9c00506fd3bfed1f4576f
maryroseeee <91707406+maryroseeee@users.noreply.github.com>
2025-08-11 12:59:37 -0400

    card

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Login from './pages/account/Login.tsx';
-import Dashboard from './pages/Dashboard.tsx';
-import Search from './pages/Search.tsx';
+import Login from './pages/account/Login';
+import Dashboard from './pages/Dashboard';
+import Search from './pages/Search';
 import './App.css';
 
 export default function App() {
